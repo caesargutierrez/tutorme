c34bf6502a9982062b4428d2d1ecc172a4946a37
caesargutierrez <karim-100gtz@hotmail.com>
2020-10-27 12:23:35 -0500

    environemnt set up

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
+from flask import Flask,request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
