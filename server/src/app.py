9fc76821b6d5bb4e7e1c3ca78e228a58f8f473d2
caesargutierrez <karim-100gtz@hotmail.com>
2020-11-23 18:09:13 -0600

    Most style and presentation done, needs backend and deployment

---
+++ server/src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask,request,render_template, make_response
+import os
+import time
+app = Flask(__name__,static_folder='../../static')
+
+@app.route('/')
+def index():
+    context = { 'server_time': format_server_time() }
+    template = render_template('index.html', context=context)
+    response = make_response(template)
+    response.headers['cache-Control'] = 'public, max-age=300, s-maxage=600'
+    return 
+
+def format_server_time():
+    server_time = time.localtime()
+    return time.strftime("%I:%M:%S %p", server_time)
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
